f5f40b685b267e04cc5e597a1af549d2d79e71c9
fidoaf <fidoaf@gmail.com>
2022-10-12 08:36:43 +0200

    Styled homepage

--- generate.bat
+++ generate.bat
@@ -1,3 +1,4 @@
+del bin\core.min.js bin\script.min.js bin\style.min.css
 uglifyjs --compress --mangle -- p5.js ml5.js > bin\core.min.js
 uglifyjs --compress --mangle -- effects.js sketch.js > bin\script.min.js
 uglifycss style.css > bin\style.min.css
